f54b23a3255b62b02a18db34961d472432cc54a2
sandr463 <stanish2000@gmail.com>
2019-03-19 10:27:28 +0200

    Add login via facebook/google

--- My_blog/settings.py
+++ My_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*@jo$n2e6%y=-1okh0802866#k+f=vnf0t!om8bl)u@jk064g7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.0.107']
+ALLOWED_HOSTS = ['localhost', '192.168.0.107', 'example.com']
 
 
 # Application definition
@@ -38,6 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'django.contrib.sites',
+
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    'allauth.socialaccount.providers.google',
+    'allauth.socialaccount.providers.facebook',
 ]
 
 MIDDLEWARE = [
@@ -69,7 +77,55 @@ TEMPLATES = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = (
+     # Needed to login by username in Django admin, regardless of `allauth`
+     'django.contrib.auth.backends.ModelBackend',
+
+     # `allauth` specific authentication methods, such as login by e-mail
+     'allauth.account.auth_backends.AuthenticationBackend',
+ )
+
 WSGI_APPLICATION = 'My_blog.wsgi.application'
+
+
+SOCIALACCOUNT_PROVIDERS = {
+    'facebook': {
+        'METHOD': 'oauth2',
+        'SCOPE': ['email', 'public_profile', 'user_friends'],
+        'AUTH_PARAMS': {'auth_type': 'reauthenticate'},
+        'INIT_PARAMS': {'cookie': True},
+        'FIELDS': [
+            'id',
+            'email',
+            'name',
+            'first_name',
+            'last_name',
+            'verified',
+            'locale',
+            'timezone',
+            'link',
+            'gender',
+            'updated_time',
+        ],
+        'EXCHANGE_TOKEN': True,
+        'LOCALE_FUNC': 'path.to.callable',
+        'VERIFIED_EMAIL': False,
+        'VERSION': 'v2.12',
+    }
+}
+
+#facebook
+SOCIAL_AUTH_FACEBOOK_KEY = '329890761203600'  # App ID
+SOCIAL_AUTH_FACEBOOK_SECRET ='e883a2b16325e7f7028da88201b7d29f'  # app key
+
+LOGIN_REDIRECT_URL = "/"
+# if you succeed in login, you'll be redirected to the main page.
+
+#site id
+SITE_ID = 1 # for the dev mode, you need to use localhost's id facebook does not support the name 127.0.0.1:8000
+# little options for your page's signup.
+ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_USERNAME_REQURIED=True
 
 
 # Database
@@ -114,7 +170,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
