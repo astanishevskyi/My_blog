d855a710c0901453766df51d658d99a3d5b6e2b5
sandr463 <stanish2000@gmail.com>
2019-05-16 15:19:19 +0300

    Features

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,9 @@ class Article(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
     text = models.TextField(null=False, blank=False)
     title = models.CharField(max_length=100, null=False, blank=False)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=None)
+    text = models.TextField(null=False, blank=False)
+    
